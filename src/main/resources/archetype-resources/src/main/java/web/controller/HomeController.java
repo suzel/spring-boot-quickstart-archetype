86f31422518efa00ed13bee05c03f32163976213
Sukru Uzel <sukru.uzel@gmail.com>
2015-07-13 17:51:49 +0300

    Update welcome message.

---
+++ src/main/resources/archetype-resources/src/main/java/web/controller/HomeController.java
@@ -0,0 +1,18 @@
+package ${package}.web.controller;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HomeController {
+	
+	private static final String HOME_VIEW_NAME = "index";
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(Principal principal) {
+		return HOME_VIEW_NAME;
+	}
+
+}
---
+++ src/main/resources/archetype-resources/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Spring Boot</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>
