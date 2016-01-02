0fddbb58ff2c9feb105725640b68138375f129d5
suzel <sukru.uzel@gmail.com>
2016-01-02 11:03:45 +0200

    Removed docker

--- src/main/resources/archetype-resources/src/main/docker/docker.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-eval "$(docker-machine env default)"
-
-docker stop $(docker ps -a -q)
-docker-compose stop
-
-docker-compose -p ${artifactId.toLowerCase().replace("-", "")} up -d
