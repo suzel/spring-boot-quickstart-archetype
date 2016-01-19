f095b54b94f85f769657145f333bff115428fd01
suzel <sukru.uzel@gmail.com>
2016-01-19 20:01:45 +0200

    Updated package.json

--- src/main/resources/archetype-resources/src/main/resources/static/Gruntfile.js
+++ src/main/resources/archetype-resources/src/main/resources/static/Gruntfile.js
@@ -1,1 +1,18 @@
 'use strict';
+
+module.exports = function (grunt) {
+
+  require('time-grunt')(grunt);
+
+  // Config
+  grunt.initConfig({});
+
+  // Task : default
+  grunt.registerTask('default', function () {
+  });
+
+  // Task : test
+  grunt.registerTask('test', function () {
+  });
+
+};
